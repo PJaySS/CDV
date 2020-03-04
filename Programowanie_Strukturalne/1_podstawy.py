0261180a2c8dbc49b331fedb3d23b5c680420888
Adam Różycki <arozycki@edu.cdv.pl>
2020-03-04 15:28:27 +0100

    first lesson

--- Programowanie_Strukturalne/1_podstawy.py
+++ Programowanie_Strukturalne/1_podstawy.py
@@ -1,1 +1,45 @@
 print("CDV")
+print(10)
+'''
+komentarz
+blokowy
+'''
+
+#potęgowanie
+
+potega=2 ** 10
+print(potega)
+
+#pobieranie danych z klawiatury
+name=input()
+print("Twoje imię: "+name)
+surname=input()
+print("Twoje imię: "+name+", nazwisko: "+surname)
+
+'''
+Użytkownik podaje z klawiatury swój wiek.
+Wyświetl dane w formacie: Twój wiek: ... lat
+'''
+
+print("Podaj swój wiek: ", end="")
+age=input()
+print("Twój wiek: ",age,"lat(a)")
+
+age1=20
+print(type(age1))
+
+
+surname="Kowalski"
+firstLetter=surname[0]
+print(firstLetter)
+
+length = len(surname)
+print(length)
+
+lastLetter=surname[len(surname)-1]
+print(lastLetter)
+
+lastLetter=surname[-1]
+print(lastLetter)
+
+#konwersja
